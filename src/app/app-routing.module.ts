c671a9e1b766417801ddb70883d475485c0a2f72
Mateus da Silva Sousa Dias <41194778+mateuussilvapb@users.noreply.github.com>
2022-09-09 13:16:23 -0300

    Projeto finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { SobreComponent } from './core/pages/components/sobre/sobre.component';
+import { HomeComponent } from './core/pages/components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
